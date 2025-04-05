9a129a15b8b49f207fb5e8aaf25f6b085b1f98c0
MoKoCo <137850094+MoKoCoBall@users.noreply.github.com>
2025-04-05 15:00:07 +0900

    [Gold III] Title: IQ Test, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1111. IQ Test/IQ Test.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int N;
+  cin >> N;
+
+  vector<int> V(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> V[i];
+  }
+
+  if (V.size() == 1) {
+    cout << "A";
+    return 0;
+  } else if (V.size() == 2) {
+    if (V[0] == V[1]) {
+      cout << V[0];
+      return 0;
+    } else {
+      cout << "A";
+      return 0;
+    }
+  } else {
+    int a = V[1] - V[0] == 0 ? 0 : (V[2] - V[1]) / (V[1] - V[0]);
+    int b = V[1] - V[0] * a;
+
+    for (int i = 0; i < N - 1; i++) {
+      if (V[i + 1] != V[i] * a + b) {
+        cout << "B";
+        return 0;
+      }
+    }
+
+    cout << V[N - 1] * a + b;
+  }
+
+  return 0;
+}
