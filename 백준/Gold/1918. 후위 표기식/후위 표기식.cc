71a65882e0c9ccf9be86e01f431e95b032805c60
MoKoCo <137850094+MoKoCoBall@users.noreply.github.com>
2025-04-27 17:47:21 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int compare(char c) {
+  if (c == '(') return 0;
+  if (c == '+' || c == '-') return 1;
+  if (c == '*' || c == '/') return 2;
+  return 3;
+}
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  string exp;
+  cin >> exp;
+
+  stack<char> S;
+
+  for (char c : exp) {
+    if (c >= 'A' && c <= 'Z') {
+      cout << c;
+    } else if (c == '(') {
+      S.push(c);
+    } else if (c == ')') {
+      while (S.top() != '(') {
+        cout << S.top();
+        S.pop();
+      }
+      S.pop();
+    } else {
+      while (!S.empty() && compare(S.top()) >= compare(c)) {
+        cout << S.top();
+        S.pop();
+      }
+      S.push(c);
+    }
+  }
+
+  while (!S.empty()) {
+    cout << S.top();
+    S.pop();
+  }
+
+  return 0;
+}
